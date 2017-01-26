91591a5b2a4d8ecffdc52ad65a629da2c1cb69f3
Hussain Jasim <hussain@archimedes>
2017-01-26 18:25:59 -0500

    redid mandelbrot rendering

--- mandelbrot.js
+++ mandelbrot.js
@@ -1,173 +1,168 @@
-// ====== helper functions for handling complex quantities ======
-
-// generating function for the mandelbrot set := z^2+c
-// z^2 = (x+iy)^2 = (x^2-y^2)+2ixy = (x-y)(x+y)+2ixy
-function mbgen_r(zr, zi, cr) {
-	return ((zr-zi)*(zr+zi))+cr;
-}
-function mbgen_i(zr, zi, ci) {
-	return (2*zr*zi)+ci;
-}
-
-// complex product z*w
-// z*w = (x+iy)*(u+iv) = (xu-yv)+i(xv+yu)
-function cprod_r(zr, zi, wr, wi) {
-	return (zr*wr)-(xi*wi);
-}
-
-function cprod_i(zr, zi, wr, wi) {
-	return (zr*wi)+(zi*wr);
+// check for z being analytically known to be in the Mandelbrot set
+function analytically_in_mb(zr, zi, qr, qi, mod) {
+	if(zr == 0 && zi == 0) {
+		return true;
+	}
+	// cardioid test
+	var q = mod-zr/2+1/16;
+	if(q*(q+zr-1/4)-qi/4 < 0) {;
+		return 1;
+	}
+	// period 2 bulb
+	if(mod+zr+15/16 < 0) {
+		return 1;
+	}
+	return 0;
 }
 
-// squared modulus of z = x+iy := z*z| = x^2+y^2
-function cmodsq(zr, zi) {
-	return (zr*zr)+(zi*zi);
-}
-
-function analytically_in_mb(zr, zi) {
-	return false;
-}
-
-
-// ====== mandelbrot set ======
-
-/* @mb_gen_data(params):
- * @ scale: a positive number that is used to determine how zoomed in the mandelbrot set is
- * @xres: width of view in pixels
- * @yres: height of view in pixels
- * @orgr: real part of the origine (where the mandelbrot set is centred)
- * @orgi: imaginary part of the origine
- * @maxiter: maximum number of iterations to test
- *
- * returns a data object with histogram, and view fields:
- * histogram is an array of length maxiter that tracks the number of values that escaped for each iteration
- * view is a 2d array of dimensions yres * xres with 0, 0 at the top left that tracks the number of iterations it takes each pixel to escape (0 for non-escaping)
-*/
-function mb_gen_data(scale, xres, yres, orgr, orgi, maxiter, rot) {
-	if(scale < 1) {
+// iterate a single point z up to maxiter times,
+// returning 0 if z does not diverge,
+// or a fractional normalized escape count
+function iterate_point(cr, ci, squared_escape, maxiter, log) {
+	// iteration value
+	var zr = cr;
+	var zi = ci;
+	// squares of iteration value
+	var qr = cr*cr;
+	var qi = ci*ci;
+	// modulus of iteration value
+	var mod = qr+qi;
+	if(analytically_in_mb(zr, zi, qr, qi, mod)) {
 		return 0;
 	}
-	offset = 1.0 * cmodsq(orgr, orgi);
-	if(offset >= 4.0) {
-		return 0;
+	var iter = 2; // the first iteration gives cr+i*ci as assigned already
+	while(1) {
+		if(iter > maxiter) {
+			break;
+		}
+		zr = qr-qi+cr;
+		zi = zi*zr;
+		zi = zi+zi+ci;
+		qr = zr*zr;
+		qi = zi*zi;
+		mod = qr+qi;
+		if(analytically_in_mb(zr, zi, qr, qi, mod)) {
+			return 0;
+		}
+		if(mod > squared_escape) { // z diverges
+			return (iter+2-log(log(mod))/log(2))/maxiter;
+		}
+		iter += 1;
+	}
+	return 0;
+}
+
+// maps 0<hue<=1 to HSV space of 0<h<360, s=1, v=1 linearly
+// (as a 2pi*hue rotation around the cylinder boundary)
+function colorize(buff, ptr, hue, min, interval) {
+	ptr *= 4; // change ptr from 8 to single byte representation, then divide by 2 because the pixel values are only in the first half of buff
+	buff[ptr+3] = 255; // full alpha value for everything
+	if(hue == 0) {
+		buff[ptr] = 0;
+		buff[ptr+1] = 0;
+		buff[ptr+2] = 0;
+		return;
+	}
+	hue -= min;
+	hue /= interval;
+	hue *= 767; // 3*256-1
+	hue = hue|0;
+	if(hue < 256) { // red-blue sector
+		buff[ptr] = 255-hue;
+		buff[ptr+1] = hue;
+		buff[ptr+2] = 0;
+	} else if(hue < 512) { // blue-green sector
+		buff[ptr] = 0;
+		buff[ptr+1] = 511-hue;
+		buff[ptr+2] = hue-256;
+	} else { // green-red sector
+		buff[ptr] = hue-512;
+		buff[ptr+1] = 0;
+		buff[ptr+2] = 767-hue;
+	}
+	return;
+}
+
+// buff is a byte buffer that is 8 the sice of the number of pixels:
+// each pixel needs 8 bytes for the double precision hue value.
+// The computed pixel values will be written to the first half of the buffer,
+// since only 4 bytes (RGBA) are needed for each.
+// xres, yres are the pixel dimensions of the view.
+// Scale>=1 is a zoom value that just divides the effective radius.
+// orgr, orgi are the complex coordinates of the center of the view.
+// Squared_escape is the square of the escape radius.
+// Maxiter is the maximum iteration count.
+// Rot is an angle that is optionally used to take rotated coordinates.
+function draw_mandelbrot_image(buff, xres, yres, scale, orgr, orgi, squared_escape, maxiter, rot) {
+	var r = 0;
+	if(scale < 1) {
+		return;
+	}
+	var offset = (orgr*orgr) + (orgi*orgi);
+	if(offset > 4) {
+		return;
 	}
 	// set the radius
-	if(offset == 0.0) {
-		r = 2.0;
+	if(offset == 0) {
+		r = 2;
 	} else {
 		r = 2-Math.sqrt(offset);
 	}
-	r /= (scale);
-	xstep = 2*r/xres;
-	ystep = 2*r/yres;
+	r = r/scale;
+	var xstep = 2*r/xres;
+	var ystep = 2*r/yres;
 	// only continue if the resolution isn't too fine for floating point precision
-	if(xstep == 0.0 || ystep == 0.0) {
-		return 0;
+	if(xstep == 0 || ystep == 0) {
+		return;
 	}
-//	hist = new Array(maxiter);
-	view = new Array(yres);
-	// initialize hist to all 0
-//	for(i = 0; i < maxiter; i++) {
-//		hist[i] = 0;
-//	}
-	// do the mandelbrot iteration for each pixel in view
-	// vy, vx are pixels in view, pi, pr are points on complex plane being considered
-	for(vy = 0, ci = orgi+r; vy < yres; vy++, ci -= ystep) {
-		// initialize row of view
-		view[vy] = new Array(xres);
-		for(i = 0; i < xres; i++) {
-			view[vy][i] = 0;
+	// Iteration setup:
+	var view_64 = new Float64Array(buff);
+	var dptr = 0; // pointer to 64-bit buffer view
+	// values used for rotations:
+	var actual_cr = 0, actual_ci = 0;
+	var rotr = 0, roti = 0;
+	// precalculate the first x-coordinate value so it isn't recalculated for each row
+	var begin_cr = orgr-r;
+	// hue value tracking
+	var hue = 0;
+	var min_hue = 1;
+	var max_hue = 0;
+	var hue_interval = 0;
+	// loop variables: vy, vx for pixel positions; ci, cr for complex coordinates
+	var vy, vx, ci, cr;
+	// precache Math.log so it's reference isn't searched for every iteration
+	var log = Math.log;
+	// First pass: store hue values in buff and track min and max hue values.
+	for(vy = yres, ci = orgi+r; vy--; ci -= ystep) {
+		for(vx = xres, cr = begin_cr; vx--; cr += xstep) {
+		actual_cr = cr;
+		actual_ci = ci;
+		if(rot > 0) { // rotate coordinate values rot radians with a complex multiplication by e^(i*rot)
+			rotr = Math.cos(rot);
+			roti = Math.sin(rot);
+			actual_cr = (cr*rotr)-(ci*roti);
+			actual_ci = (cr*roti) + (ci*rotr);
 		}
-		for(vx = 0, cr = orgr-r; vx < xres; vx++, cr += xstep) {
-			actual_cr = cr;
-			actual_ci = ci;
-			if(rot > 0.0) {
-				rotr = Math.cos(rot);
-				roti = Math.sin(rot);
-				actual_cr = (cr*rotr)-(ci*roti);
-				actual_ci = (cr*roti)+(ci*rotr);
-			}
-			// do the mandelbrot iteration for c = pr+i*pi
-			// iterated value tracking
-			zr = actual_cr;
-			zi = actual_ci;
-			// squared values
-			qr = zr*zr;
-			qi = zi*zi;
-			// modulus of z
-			mod = 0;
-			for(iter = 1; iter < maxiter; iter++) {
-				zr = qi-qr+actual_cr;
-				zi = (2*zr*zi)+actual_ci;
-				qr = zr*zr;
-				qi = zi*zi;
-				if(analytically_in_mb(zr, zi)) {
-					break;
-				}
-				mod = qr+qi;
-				if(mod > 4000) {
-					view[vy][vx] = 1-(Math.log(Math.log(mod))/(iter*Math.LN2));
-//					hist[iter]++;
-//					hist[0]++;
-					break;
-				}
-			}
+		hue = iterate_point(actual_cr, actual_ci, squared_escape, maxiter, log);
+		view_64[dptr] = hue;
+		dptr += 1;
+		if(hue < min_hue) {
+			min_hue = hue;
+		}
+		if(hue > max_hue) {
+			max_hue = hue;
 		}
 	}
-	return {
-//		"histogram" : hist,
-		"view" : view,
-	};
-}
-
-function draw_mandelbrot_image(mbdata, ctx) {
-	view = mbdata.view;
-	//hist = mbdata.histogram;
-	length = view.length;
-	width = view[0].length;
-	image = new Uint8ClampedArray(4*width*length);
-/*
-	iter_min = 0;
-	iter_max = 0;
-	for(i = 1; i < hist.length; i++) {
-		if(hist[i] > 0) {
-			iter_min = i;
-			break;
-		}
 	}
-	for(i = hist.length-1; i >= iter_min; i--) {
-		if(hist[i] > 0) {
-			iter_max = i;
-			break;
-		}
+	// Hue values are normalized so that they span the entire (0,1] interval.
+	// The normalization is linear.
+	// This is important for very zoomed images, where the raw hue distribution has a varience that is too small to give different colors.
+	hue_interval = max_hue - min_hue;
+	var dptr_max = xres*yres;
+	var view_u8 = new Uint8Array(buff); // view for RGBA pixel components
+	// Second pass: write normalized color values to the buffer.
+	for(dptr = 0; dptr < dptr_max; dptr++) {
+		colorize(view_u8, dptr, view_64[dptr], min_hue, hue_interval);
 	}
-	iter_accum = hist[0]-hist[iter_min];
-	hist[iter_min] = 0.0;
-	for(i = iter_min+1; i <= iter_max; i++) {
-		hist[i] /= iter_accum;
-		hist[i] += hist[i-1];
-	}
-*/
-	// tracking position in image data
-	ipos = 0;
-	for(y = 0; y < length; y++) {
-		for(x = 0; x < width; x++) {
-			// mandelbrot view of pixel
-			v = view[y][x];
-			if(v == 0) {
-				color = 0;
-				ipos += 3;
-			} else {
-				hue = 12.0-(12.0*Math.pow(v, 0.95));
-				image[ipos++] = Math.round(255*(1-Math.min(hue/3.0, 1.0))); // red
-				hue = Math.min(hue-3.0, 0.0);
-				image[ipos++] = Math.round(255*(1-Math.min(hue/4.0, 1.0))); // green
-				hue = Math.min(hue-4.0, 0.0);
-				image[ipos++] = Math.round(255*(1-Math.min(hue/5.0, 1.0))); // blue
-			}
-			// set alpha to full for everything
-			image[ipos++] = 255;
-		}
-	}
-	ctx.putImageData(new ImageData(image, width, length), 0, 0);
+	return;
 }
